38c2956bd4aade5a5e6c1fad1bfc4e24ac69c94e
mondli16 <mondlik313@gmail.com>
2026-08-26 18:02:29 +0200

    fetch data from the backend

--- 7 Backend and Data Fetching/ecommerce-project/src/pages/HomePage.jsx
+++ 7 Backend and Data Fetching/ecommerce-project/src/pages/HomePage.jsx
@@ -2,7 +2,18 @@ import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 export function HomePage() {
-
+// fetch('http://localhost:3000/api/products')
+//     .then((response)=>{
+//       response.json().then((data)=>{
+//         console.log(data)
+//       });
+//     })
+fetch('http://localhost:3000/api/products')
+    .then((response)=>{
+      return response.json();
+    }).then((data)=>{
+        console.log(data)
+    });
   return (
     <>
       <title>Ecommerce Project</title>
