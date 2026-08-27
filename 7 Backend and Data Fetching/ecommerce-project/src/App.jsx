81a526a0d64c055af5df888ab4953a43244636b4
mondli16 <mondlik313@gmail.com>
2026-08-27 18:48:05 +0200

    Generate HYML for cart items

--- 7 Backend and Data Fetching/ecommerce-project/src/App.jsx
+++ 7 Backend and Data Fetching/ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import { HomePage } from './pages/HomePage'
 import {CheckoutPage} from './pages/CheckoutPage'
 import './App.css'
@@ -6,12 +8,17 @@ import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
-
+  const [cart, setCart] = useState([]);
+  useEffect(()=>{
+      axios.get('/api/cart-items').then((response)=>{
+  setCart(response.data);
+  })
+  })
   return (
     <>
     <Routes>
-      <Route index element={ <HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={ <HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
