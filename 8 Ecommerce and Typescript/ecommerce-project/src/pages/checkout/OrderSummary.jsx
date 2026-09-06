f7fc291cabf8ab43ddea825bbd957fd2cf786679
mondli16 <mondlik313@gmail.com>
2026-09-06 08:00:25 +0200

    Create new React TypeScript project

---
+++ 8 Ecommerce and Typescript/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,52 @@
+import { formatMoney } from "../../utils/money";
+import axios from "axios";
+import { DeliveryOptions } from "./DeliveryOptions";
+export function OrderSummary({cart, deliveryOptions, loadCart}){
+    return(
+        <div className="order-summary">
+            {cart.map((cartItem) => {
+              const deleteCartItem = async() =>{
+                await axios.delete(`/api/cart-items/${cartItem.productId}`);
+                await loadCart();
+              }
+              return (
+                <div key={cartItem.productId} className="cart-item-container">
+                  <div className="delivery-date">
+                    Delivery date: Tuesday, June 21
+                  </div>
+
+                  <div className="cart-item-details-grid">
+                    <img className="product-image"
+                      src={cartItem.product.image} />
+
+                    <div className="cart-item-details">
+                      <div className="product-name">
+                        {cartItem.product.name}
+                      </div>
+                      <div className="product-price">
+                        {formatMoney(cartItem.product.priceCents)}
+                      </div>
+                      <div className="product-quantity">
+                        <span>
+                          Quantity: <span className="quantity-label">
+                            {cartItem.quantity}
+                          </span>
+                        </span>
+                        <span className="update-quantity-link link-primary">
+                          Update
+                        </span>
+                        <span className="delete-quantity-link link-primary"
+                        onClick={deleteCartItem}>
+                          Delete
+                        </span>
+                      </div>
+                    </div>
+                    <DeliveryOptions deliveryOptions={deliveryOptions} cartItem = {cartItem} loadCart={loadCart}/>
+                  </div>
+                </div>
+              )
+            })
+            }
+          </div>
+    )
+}
---
+++ 8 Ecommerce and Typescript/ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+import { Header } from '../../components/Header';
+import './HomePage.css';
+import { ProductGrid } from './ProductGrid';
+export function HomePage({ cart, loadCart }) {
+// fetch('http://localhost:3000/api/products')
+//     .then((response)=>{
+//       response.json().then((data)=>{
+//         console.log(data)
+//       });
+//     })
+
+//This is not clean
+// fetch('http://localhost:3000/api/products')
+//     .then((response)=>{
+//       return response.json();
+//     }).then((data)=>{
+//         console.log(data)
+//     });
+
+const [products, setProducts] = useState([]);
+useEffect(()=>{
+  const getHomeData = async () => {
+     const response = await axios.get('/api/products?expand=product');
+     setProducts(response.data)
+    }
+  getHomeData();
+}, []);
+  
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <Header cart={cart}/>
+      <div className="home-page">
+        <ProductGrid products={products} loadCart={loadCart}/>
+      </div>
+    </>);
+}   
