0316c76f94e9f543cecc9f2b9b62303257eba1a4
mondli16 <mondlik313@gmail.com>
2026-09-07 14:31:23 +0200

    Convert money utils and header to typescript

--- 8 Ecommerce and Typescript/ecommerce-project-ts/src/components/Header.tsx
+++ 8 Ecommerce and Typescript/ecommerce-project-ts/src/components/Header.tsx
@@ -1,7 +1,14 @@
 import './Header.css'
 import { Link } from 'react-router'
 
-export function Header({cart}) {
+type HeaderProps = {
+    cart:{
+        productId: string,
+        quantity: number,
+        deliveryOptionId: string
+    }[];
+    };
+export function Header({cart}:HeaderProps) {
     let totalQuantity = 0;
     cart.forEach(carItem => {
         totalQuantity += carItem.quantity;
